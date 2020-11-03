962a965dcbd5dd429d1d36015c774c8b8f28a17f
acho01 <archil.sharashenidze@azry.com>
2020-11-03 22:24:30 +0400

    overlay with info text added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,39 @@
-console.log("AAAAAAAAAAAAAAAAAAAAA");
-// document.getElementById('button').addEventListener("click", function() {
-//     document.querySelector('.bg-modal').style.display = "flex";
-// });
-//
-// document.querySelector('.close').addEventListener("click", function() {
-//     document.querySelector('.bg-modal').style.display = "none";
-// });
+
+showOverlay();
+homePageTyper();
+document.querySelector("#overlay").addEventListener("click", () => hideOverlay());
+
+
+
+function homePageTyper(){
+    let welcomeMessage = 'Hello my curious alien fella.\n' +
+        '                Welcome to oop modern class.\n' +
+        '                With this tool, you can graph classes\n' +
+        '                and see the relations between them.\n' +
+        '                OOP practice has never been that fun.\n' +
+        '                Good luck :) \n' +
+        '                \n' +
+        '                Click anywhere on screen and start graphing\n' +
+        '                .\n' +
+        '                .\n' +
+        '                .\n';
+
+    type(welcomeMessage, 'overlay_text');
+}
+
+function type(text, elementId) {
+    let i = 0;
+    if (i < text.length) {
+        document.querySelector(`#${elementId}`).innerHTML += text.charAt(i);
+        i++;
+        setTimeout(() => type(text.substr(1), elementId), 45);
+    }
+}
+
+function hideOverlay() {
+    document.querySelector("#overlay").style.display='none';
+}
+
+function showOverlay() {
+    document.querySelector("#overlay").style.display='block';
+}
